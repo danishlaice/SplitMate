409e864dd7f13f4f4e255f6618d7780cb804cb34
Danish <danishlaice@gmail.com>
2026-07-28 15:40:15 +0530

    Added settlement suggestions

--- controllers/balanceController.js
+++ controllers/balanceController.js
@@ -47,10 +47,50 @@ const calculateBalance = async (req, res) => {
       balances[id].balance =
         balances[id].paid - balances[id].owes;
     });
+    // Settlement Suggestions
+const payers = [];
+const receivers = [];
+
+Object.values(balances).forEach((user) => {
+  if (user.balance < 0) {
+    payers.push({
+      name: user.name,
+      amount: Math.abs(user.balance),
+    });
+  } else if (user.balance > 0) {
+    receivers.push({
+      name: user.name,
+      amount: user.balance,
+    });
+  }
+});
+
+const settlements = [];
+
+let i = 0;
+let j = 0;
+
+while (i < payers.length && j < receivers.length) {
+  const amount = Math.min(payers[i].amount, receivers[j].amount);
+
+  settlements.push({
+    from: payers[i].name,
+    to: receivers[j].name,
+    amount: Number(amount.toFixed(2)),
+  });
+
+  payers[i].amount -= amount;
+  receivers[j].amount -= amount;
+
+  if (payers[i].amount < 0.01) i++;
+  if (receivers[j].amount < 0.01) j++;
+}
 
     res.status(200).json({
       success: true,
       balances,
+      settlements,
+      
     });
 
   } catch (error) {
